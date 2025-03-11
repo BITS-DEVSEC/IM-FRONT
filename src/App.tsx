4a2f3954debae0ea523263e495b3a42480f002c2
mikaelendale <149874638+mikaelendale@users.noreply.github.com>
2025-03-11 05:05:54 +0300

    done

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import { ThemeProvider } from "@/components/theme-provider"
 
 function App() {
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
       <Router>
         <Routes>
           <Route path="/" element={<Login />} />
